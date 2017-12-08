02239db4d5b95d2a477af014a5ea7da107fa0421
LouisAtNantes <louis.bremand@gmail.com>
2017-12-08 15:20:05 +0100

    Revert "Solution SQL injection"
    This reverts commit 369ba0883268ba94b40f8ac5d4202b503ac54a24.

--- webgoat-lessons/sql-injection/src/main/java/org/owasp/webgoat/plugin/introduction/SqlInjectionLesson5a.java
+++ webgoat-lessons/sql-injection/src/main/java/org/owasp/webgoat/plugin/introduction/SqlInjectionLesson5a.java
@@ -59,13 +59,12 @@ public class SqlInjectionLesson5a extends AssignmentEndpoint {
     protected AttackResult injectableQuery(String accountName) {
         try {
             Connection connection = DatabaseUtilities.getConnection(getWebSession());
-            String query = "SELECT * FROM user_data WHERE last_name = :lastName";
+            String query = "SELECT * FROM user_data WHERE last_name = '" + accountName + "'";
 
             try {
-                PreparedStatement statement = connection.prepareStatement(query, ResultSet.TYPE_SCROLL_INSENSITIVE,
+                Statement statement = connection.createStatement(ResultSet.TYPE_SCROLL_INSENSITIVE,
                         ResultSet.CONCUR_READ_ONLY);
-                statement.setString(1, accountName);
-                ResultSet results = statement.executeQuery();
+                ResultSet results = statement.executeQuery(query);
 
                 if ((results != null) && (results.first())) {
                     ResultSetMetaData resultsMetaData = results.getMetaData();
